ec6f97104ad222ee49f9030f85282b920f4c2e14
Nikolas <nikolas@conectra.net.br>
2020-06-25 18:31:11 -0300

    [ADD] conceitos react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      "url": "https://github.com/Rocketseat/umbriel",
+      "title": "Umbriel",
+      "techs": ["Node", "Express", "TypeScript"]
+    });
+
+    const repositorie = response.data;
+
+    setRepositories([...repositories, repositorie]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const response = await api.delete('repositories/' + id);
+
+    if(response && response.status !== 204){
+      console.log("ERRO AO EXCLUIR")
+    }
+    const newRepositories = repositories.filter(repository => repository.id !== id);
+    setRepositories(newRepositories);
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repositories.map(repositorie =>
+            {return <li key={repositorie.id}>
+              {repositorie.title}
+    
+              <button onClick={() => handleRemoveRepository(repositorie.id)}>
+                Remover
+              </button>
+            </li>
+          })
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
